5541c3d2ab299e0e1a9e23b745beabe20b161ed0
Ragnhild Elise Kavli <ragnhiek@ntnu.no>
2019-03-20 16:08:36 +0100

    the last one was had a typo

--- Main.java
+++ Main.java
@@ -11,12 +11,7 @@ public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 		long startTime = System.nanoTime();
 		
-		String datafile = "1T5"
-				+ ""
-				+ ""
-				+ ""
-				+ ""
-				+ "R1V_java.txt";
+		String datafile = "19F5R1V_java.txt";
 		
 		File file = new File ("7R1V_java_results.txt");
 		
